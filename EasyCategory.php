6cb5df553bf927637dfbcb75e2ead14b918befc4
Alexandre Girard <git@alexgirard.com>
2014-09-29 15:22:36 +0200

    clean display

--- EasyCategory.php
+++ EasyCategory.php
@@ -83,7 +83,6 @@ class EasyCategory {
             <img src="<?=$config->thumb_url?>/<?=$video->filename?>.jpg" width="200px"/>
         </td>
         <td class="video-category">
-          <?=$video->cat_id?>
           <form method="post">
             <input type="hidden" name="easycategory_action" value="update"/>
             <input type="hidden" name="easycategory_video_id" value="<?= $video->video_id ?>"/>
