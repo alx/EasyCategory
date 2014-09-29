ddfb9c956d7c30db36643090446fbd4cdb06be2f
Alexandre Girard <git@alexgirard.com>
2014-09-29 15:07:01 +0200

    first commit

---
+++ EasyCategory.php
@@ -0,0 +1,106 @@
+<?php
+
+class EasyCategory {
+
+    static function Load() {
+    }
+
+    static function Info() {
+        return array (
+            'name'    => 'EasyCategory',
+            'author'  => 'Alexandre Girard',
+            'version' => '1.0.0',
+            'site'    => 'http://alexgirard.com/',
+            'notes'   => 'Easily change categories of all your CumulusClips videos'
+        );
+    }
+
+    static function Install() {
+    }
+
+    static function Uninstall() {
+    }
+
+    static function Settings() {
+      global $config;
+
+      App::LoadClass ('Video');
+      $db = Database::GetInstance();
+
+      if (isset ($_POST['easycategory_action']) &&  $_POST['easycategory_action'] == "update") {
+
+        $query = "SELECT * FROM " . DB_PREFIX . "videos WHERE `video_id` = " . Database::Escape($_POST['easycategory_video_id']);
+        $video_result = $db->Query ($query);
+        $video_count = $db->Count ($video_result);
+
+        if($video_count == 1) {
+          $query = "UPDATE " . DB_PREFIX . "videos SET ";
+          $query .= "`cat_id` = " . Database::Escape($_POST['easycategory_cat_id']) . " ";
+          $query .= "WHERE `video_id` = " . Database::Escape($_POST['easycategory_video_id'];
+          $db->Query ($query);
+        }
+
+      }
+
+      $query = "SELECT video_id FROM " . DB_PREFIX . "videos WHERE status = 'approved'";
+      $query .= " ORDER BY video_id DESC";
+      $result = $db->Query ($query);
+      $total = $db->Count ($result);
+
+      $categories = array();
+      // Retrieve Category names
+      $query = "SELECT cat_id, cat_name FROM " . DB_PREFIX . "categories";
+      $result = $db->Query ($query);
+      while ($row = $db->FetchObj ($result)) {
+          $categories[$row->cat_id] = $row->cat_name;
+      }
+
+?>
+
+<h1>EasyCategory</h1>
+
+<?php if ($total > 0): ?>
+<div class="block list">
+  <table>
+    <thead>
+        <tr>
+            <td class="video-title large">Title</td>
+            <td class="video-category large">Category</td>
+        </tr>
+    </thead>
+    <tbody>
+    <?php while ($row = $db->FetchObj ($result)): ?>
+
+      <?php
+        $odd = empty ($odd) ? true : false;
+        $video = new Video ($row->video_id);
+      ?>
+
+      <tr class="<?=$odd ? 'odd' : ''?>">
+        <td class="video-title">
+            <a href="<?=ADMIN?>/videos_edit.php?id=<?=$video->video_id?>" class="large"><?=$video->title?></a><br />
+            <img src="<?=$config->thumb_url?>/<?=$video->filename?>.jpg" width="200px"/>
+        </td>
+        <td class="video-category">
+          <form method="post">
+            <input type="hidden" name="easycategory_action" value="update"/>
+            <input type="hidden" name="easycategory_video_id" value="<?= $row->video_id ?>"/>
+            <?php foreach ($categories as $cat_id => $cat_name): ?>
+            <input type="radio" name="easycategory_cat_id" value="<?=$cat_id?>" <?= ($video->cat_id == $cat_id) ? 'checked' : ''?>> <?=$cat_name?><br>
+            <?php endforeach; ?>
+            <p><input value="Update" type="submit"/></p>
+          </form>
+        </td>
+      </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
+</div>
+<?php else: ?>
+<div class="block"><strong>No videos found</strong></div>
+<?php endif; ?>
+
+<?php
+    }
+}
+?>
---
+++ README.md
@@ -0,0 +1,19 @@
+CumulusClips Plugin : EasyCategory
+==================================
+
+Easily change categories for all your CumulusClips videos.
+
+# Install
+
+1. Go to your CumulusClips plugin folder
+2. ```git clone https://github.com/alx/EasyCategory.git```
+3. In CumulusClips admin interface, EasyCategory plugin from plugins panel
+
+# Usage
+
+1. In CumulusClips admin interface, go to plugins panel and click on EasyCategory settings
+2. You'll find a list of your video, for each video you can set its category
+
+# Screenshot
+
+![Screenshot](/docs/screenshot.png?raw=true)
